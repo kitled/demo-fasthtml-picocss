ac2e6810b5736a71329edf8fc819d557c1d7301e
Chris L <pro@kit.gdn>
2024-06-21 02:33:13 +0200

    Color palette implemented, with to_rgb(color,shade) function, and make_div() → make_divs() with those colors. We also referenced the default shade for each color in Pico CSS.
    For now it's all hardwired and basic but we successfully use Python to preprocess CSS using a JSON file object , essentially.

--- css_demo.py
+++ css_demo.py
@@ -1,7 +1,7 @@
-from curses import meta
 from fasthtml.common import *
-from fasthtml.js import MarkdownJS, SortableJS
-website = "FastHTML 💙 Pico CSS"
+import json
+website = "FastHTML 🧡 Pico CSS"
+html = Html(lang='en', data_theme='dark')
 
 pico_css = Link(
     rel="stylesheet", 
@@ -13,7 +13,7 @@ prism_css = Link(
     href="style/prism.css",
     type="text/css"
     )
-prism_css_theme = Link(
+prism_css_theme = Link(    # linked at the bottom of the HTML file
     rel="stylesheet", 
     href="style/prism-one-dark.css",
     type="text/css"
@@ -27,7 +27,7 @@ prism_js = Script(src="style/prism.js")
 
 # css_overrides = []
 # css = Style(" ".join(o for o in css_overrides))
-head = Meta(charset="utf-8"), Meta(name="viewport", content="width=device-width, initial-scale=1"), Meta(name="color-scheme", content="light dark"), Title(website)
+head = Meta(charset="utf-8"), Meta(name="viewport", content="width=device-width, initial-scale=1"), Meta(name="color-scheme", content="light dark")
 
 app = FastHTML(hdrs=(
     head,
@@ -36,7 +36,6 @@ app = FastHTML(hdrs=(
     prism_css_theme,
     demo_css, 
     # css,         # use for in-file custom CSS; otherwise in demo.css
-    # prism_js,    # linked at the bottom of the HTML file
     ))
 rt = app.route
 
@@ -49,7 +48,6 @@ async def get(fname:str, ext:str): return FileResponse(f'{fname}.{ext}')
 
 # Canonical HTML structure
 menu = Div(P(A("Notes", href="/log")), P(A("Demos", href="/llm")), cls='grid')
-
 
 # Our data
 starter_html_template = NotStr("""&lt;!doctype html>
@@ -69,13 +67,34 @@ starter_html_template = NotStr("""&lt;!doctype html>
 &lt;/html>""")
 # ↓
 block_code = Pre(Code(starter_html_template, cls='language-html'), cls='prismjs')
-# block_code = Pre(Code(starter_html_template, cls='line-numbers language-html'), cls='prismjs')
 
 
-
+colors = [
+    'red','pink','fuchsia','purple','violet','indigo','blue','azure','cyan','jade',
+    'green','lime','yellow','amber','pumpkin','orange','sand','grey','zinc','slate']
+shades = [
+    '550', '500', '550', '600', '600', '600', '550', '550', '550', '550', 
+    '500', '200', '100', '200', '300', '500', '200', '300', '550', '600']
 # ↓
+def to_rgb(color_name, shade):
+    with open('style/pico-color-palette.json', 'r') as file:
+        color_data = json.load(file)
+    return str(color_data[color_name][shade])
 
+def make_div(color, shade):
+    return Div(" • ", style='background:'+ to_rgb(color, shade)+';',)
 
+def make_divs(colors=colors, shades=shades):
+    divs = tuple()
+    for i in range(len(colors)):
+        divs += (make_div(colors[i], shades[i]),)
+    return divs
+
+color_palette = Article(Header(
+    make_divs(),
+    cls='grid', style='background: #000;',
+), style='background: #000;',
+)
 
 # ↓
 
@@ -87,6 +106,8 @@ section_1 = Section(
     H3("Quick start"), 
     H4("Starter HTML template"),
     block_code,
+    H3("Version picker"),
+    color_palette,
     )
 section_2 = Section(
     H2("Customization"), 
@@ -95,14 +116,16 @@ section_2 = Section(
 
 # ↓
 
-header = Header(H1(website), menu, cls='container')
+header = Header(H1(website), menu, Hr(), cls='container')
 main   = Main(section_1, section_2, cls='container line-numbers')
-footer = Footer(P("Made by kit using FastHTML & Pico CSS, June 2024."), cls='container')
+footer = Footer(Hr(),P("Made by kit using FastHTML & Pico CSS, June 2024."), cls='container')
 scripts = Script(src="style/prism.js")
 
-
+# data_theme = attrdict(dict({'data-theme':'light'}))
 # Home page (Cover page)
 @rt("/")
 def get():
-    return header, main, footer, scripts
-# , header, main, footer, bottom_html
+    return html, Title(website), header, main, footer, scripts
+
+
+# Style(':root {prefers-color-scheme: light;}')
