01ecf618a11129e2d549b173c3142bed7b34baf1
Chris L <pro@kit.gdn>
2024-06-23 03:17:27 +0200

    Reimplemented `Code()` such that all <code> blocks will be highlighted. Must now only specifiy `language-name` for each.

--- single_page.py
+++ single_page.py
@@ -1,5 +1,5 @@
 from fasthtml.common import * # type: ignore
-
+from fasthtml.js import MarkdownJS, SortableJS, HighlightJS
 
 html = Html(lang='en')
 head = (
@@ -16,26 +16,34 @@ pico_css = Link(
     href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.pumpkin.min.css", 
     type="text/css"
     )
+page_css = Link(
+    rel="stylesheet", 
+    href="style/single-page.css", 
+    type="text/css"
+    )
 
 app = FastHTML(hdrs=(
     head,
     pico_css,
+    page_css,
+    MarkdownJS('.markdown'),
+    SortableJS('.sortable'),
+    HighlightJS('.highlight'),
     ))
 rt = app.route
 @rt("/{fname:path}.{ext:static}") # Serve static files
 async def get(fname:str, ext:str): return FileResponse(f'{fname}.{ext}') # type: ignore
 
 
-
-
-# def make_section(section:dict):
-#     items = []
-#     for title, level in section.items():
-#         items.append(heading(title, level))
-#     return Section(*items)
-
-
-
+_Code = Code  # Trick! 😼
+def Code(*args, cls=None, **kwargs):
+    '''Hardcode `highlight` class.
+    '''
+    if cls:
+        cls = f"highlight {cls}"
+    else:
+        cls = "highlight"
+    return _Code(*args, cls=cls, **kwargs)
 
 
 # H2 = [
@@ -47,28 +55,6 @@ async def get(fname:str, ext:str): return FileResponse(f'{fname}.{ext}') # type:
 #     "Components",
 #     "About",
 #     ]
-
-
-
-
-
-
-# let's begin by making the H3 section page replica.
-
-
-
-
-#Let's think about
-
-
-
-
-
-
-
-
-
-
 
 # We define a bunch of functions that produce the ad hoc layout of the page.
 # Most implement the generic FastHTML function, but some are specific to the page.
@@ -86,18 +72,26 @@ def aside(*aside_tags):
 
 def heading(lv:int, title:str, desc=None):
     '''Returns a header block with title and anchor link. Followed by optional description.
-    TODO: Implement *contents to add block contents.
     '''
     h = [H1, H2, H3, H4, H5, H6]
     hn = h[lv-1]
     anchor = title.lower().replace(' ', '-')
-    return hn(
-        title, 
-        A('🔗', href='#'+anchor, id=anchor, cls='secondary', tabindex="-1")
-    ), P(desc) if desc else None
+    return (
+        hn(
+            title, 
+            A(
+                '🔗', 
+                href='#'+anchor, 
+                id=anchor, 
+                cls='secondary', 
+                tabindex="-1",
+            )
+        ), 
+        P(desc) if desc else None,
+    )
 
-def art_header(lv, title, desc=None): 
-    return heading(lv=lv, title=title, desc=desc)
+Style("""#content > section > section > p:nth-child(2) {}""")
+
 #   +
 def art_c(*c): # c for content
     return (*c, )
@@ -105,8 +99,8 @@ def art_c(*c): # c for content
 def art_footer(html, python): 
     return Footer(Pre(Code(html)), Pre(Code(python)))
 #   =
-def article(c, art_header=None, art_footer=None, **kwargs): 
-    return Card(*c, header=art_header, footer=art_footer, **kwargs)
+def article(c, hd=None, ft=None, **kwargs): 
+    return Card(*c, header=hd, footer=ft, **kwargs)
 
 
 # c: lv2_s & lv3_s contain other sections (lv3_s & lv4_s respectively); 
@@ -135,39 +129,91 @@ def main(*lv2_s, aside_tags=None, **kwargs):
 # c for content
 # d for div
 
-c_1_1_1 = P(
-    A(
-        "Download Pico", 
-        rel="noopener noreferrer", 
-        href="https://github.com/picocss/pico/archive/refs/heads/main.zip", 
-        target="_blank"
+c_1_1_1 = (
+    P(
+        A(
+            "Download Pico", 
+            rel="noopener noreferrer", 
+            href="https://github.com/picocss/pico/archive/refs/heads/main.zip", 
+            target="_blank"
+        ), 
+        """ and link """, 
+        Code("/css/pico.min.css"), 
+        """ in the """, 
+        Code("""<head>""", cls="language-html", ), 
+        """ of your website.""",
     ), 
-    """ and link """, 
-    Code("/css/pico.min.css"), 
-    """ in the """, 
-    Code("""<head>""", cls="language-html", ), 
-    """ of your website.""",
+    Pre(
+        Code(
+            """<link rel="stylesheet" href="css/pico.min.css" />""", 
+            cls="language-html", 
+        ), 
     ), 
-
+)
+#  🡇
 s_1_1_1 = section(
     c_1_1_1, 
     lv=4, title="Install manually",  
-    )
+)
+#  ＋
+c_1_1_2 = P(
+    """Alternatively, you can use """, 
+    A(
+        "jsDelivr CDN", 
+        rel="noopener noreferrer", 
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/",
+        target="_blank"
+    ),
+    """ to link """, 
+    Code("pico.min.css"), 
+    '.'
+)
+#  🡇
+s_1_1_2 = section(
+    c_1_1_2, 
+    lv=4, title="Usage from CDN",  
+)
 
+
+
+
+
+
+
+
+
+
+
+#  🡇
 # this must nest all lv4_s
 s_1_1_0 = section(
-    s_1_1_1, 
     P("There are 4 ways to get started with pico.css:"), 
+    s_1_1_1, 
     lv=3, title="Quick start", 
-    desc="""Link <code>pico.css</code> manually or via CDN for a dependency-free setup, or use NPM or Composer for advanced&nbsp;usage.""", 
-    )
+    desc=(
+        """Link """,
+        Code("pico.css"),
+         """ manually or via CDN for a dependency-free setup, or use NPM or Composer for advanced usage.""", 
+    ), 
+)
 
 
+
+
+
+
+
+
+
+#  🡇
 # lv2_s must nest all lv3_s
 s_1_0_0 = section(
     s_1_1_0, 
     lv=2, title="Getting started", 
-    )
+)
+
+
+
 
 
 # # Create H2 headings
@@ -177,9 +223,9 @@ s_1_0_0 = section(
 
 
 
-
+site = (main(s_1_0_0))
 
 # Home page
 @rt("/")
 def get():
-    return main(s_1_0_0, s_1_1_0, s_1_1_1)
+    return site
